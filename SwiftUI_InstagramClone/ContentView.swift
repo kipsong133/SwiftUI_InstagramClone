11da854ed44bb13d7f4dd65294d096aece72d095
KimWooSung <kipsong133@gmail.com>
2021-07-06 23:37:24 +0900

    Initial Commit

---
+++ SwiftUI_InstagramClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_InstagramClone
+//
+//  Created by 김우성 on 2021/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_InstagramClone/SwiftUI_InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_InstagramCloneApp.swift
+//  SwiftUI_InstagramClone
+//
+//  Created by 김우성 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
