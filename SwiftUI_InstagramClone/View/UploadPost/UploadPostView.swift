71574f7af5ce1e0c4c1281da64f3903db8dd78d0
KimWooSun <kipsong133@gmail.com>
2021-08-11 22:49:50 +0900

    feat : UploadPostView UI 구현

--- SwiftUI_InstagramClone/View/UploadPost/UploadPostView.swift
+++ SwiftUI_InstagramClone/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    
+    // UIImagePicker를 UIKit에서 가져온 후,
+    @State private var selectedImage: UIImage?
+    // SwiftUI의 Image 객체에 넣기 위한 프로퍼티
+    @State var postImage: Image?
+    @State var captionText = ""
+    
     var body: some View {
-        Text("UploadPost")
+        VStack {
+            if postImage == nil { // 이미지를 선택하지 않은 경우
+                
+                Button(action: {}, label: {
+                    Image("AddImage")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                        .overlay(
+                            Text("Photo")
+                                .font(.system(size: 25, weight: .semibold, design: .rounded))
+                                .foregroundColor(.black)
+                                .offset(y: 75)
+                        )
+                })
+                
+            } else { // 이미지를 선택한 경우
+                HStack(alignment: .top) {
+                    Image("모야")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption...", text: $captionText)
+                }.padding()
+                
+                Button(action: {}, label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                })
+                
+            }
+            
+            Spacer()
+        }
     }
 }
 
